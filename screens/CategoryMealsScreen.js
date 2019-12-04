94af098119be761185a54093cd8db6fbf297c38f
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2019-12-04 16:09:08 +0200

    Creating components and basic navigation object

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native';
+
+const CategoryMealsScreen = (props) => {
+    return (
+        <View>
+            <Text>Category Meals Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default CategoryMealsScreen
---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native';
+
+const MealDetailScreen = (props) => {
+    return (
+        <View>
+            <Text>The Meal Details Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default MealDetailScreen

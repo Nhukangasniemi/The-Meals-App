241a32aa4b186b6671c9407940f48a0f421f4a06
Bilot Xamarin development <bilotdevappleid@bilot.fi>
2019-12-05 16:11:49 +0200

    Create Tab Navigator and add stack navigator as nested screen

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,14 @@
+import React from 'react'
 import { Platform } from 'react-native'
 import { createAppContainer } from 'react-navigation'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
 import { createStackNavigator } from 'react-navigation-stack'
+import {Ionicons} from '@expo/vector-icons'
 import CategoriesScreen from './../screens/CategoriesScreen';
 import CategoryMealsScreen from './../screens/CategoryMealsScreen';
 import MealDetailScreen from './../screens/MealDetailScreen';
 import Colors from '../constants/Colors'
+import FavoritesScreen from '../screens/FavoritesScreen'
 
 const MealsNavigator = createStackNavigator({
     Categories: {
@@ -23,4 +27,21 @@ const MealsNavigator = createStackNavigator({
         }
     })
 
-export default createAppContainer(MealsNavigator)
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: {screen: MealsNavigator, navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+            return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />
+        }
+    }},
+    Favorites: {screen: FavoritesScreen, navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+            return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
+        }
+    }}
+}, {
+    tabBarOptions: {
+        activeTintColor: Colors.accentColor
+    }
+})
+
+export default createAppContainer(MealsFavTabNavigator)

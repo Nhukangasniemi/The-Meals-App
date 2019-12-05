9f64e26d9224b43354789ff4183f656fef863630
Bilot Xamarin development <bilotdevappleid@bilot.fi>
2019-12-05 15:37:55 +0200

    Install HeaderButton package and setup Star icon button

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import CustomHeaderButton from '../components/HeaderButton'
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam("mealId")
@@ -22,7 +24,10 @@ MealDetailScreen.navigationOptions = (navigationData) => {
   const mealId = navigationData.navigation.getParam("mealId")
   const selectedMeal = MEALS.find(meal => meal.id === mealId)
   return {
-    headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+      <Item title="Favorite" iconName='ios-star' onPress={() => console.log("Mark as favorite")} />
+    </HeaderButtons>
   }
 }
 
